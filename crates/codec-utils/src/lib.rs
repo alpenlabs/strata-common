1ee008adb448c2691ae0efceae3f1605c07f9dd4
Trey Del Bonis <trey@alpenlabs.io>
2026-08-12 13:00:35 +0000

    feature(codec-utils): add `ChunkIterDecoder` (#168)

--- crates/codec-utils/src/lib.rs
+++ crates/codec-utils/src/lib.rs
@@ -1,7 +1,9 @@
 //! Utils around the strata-codec system that don't belong in the upstream crates.
 
 mod borsh_shim;
+mod chunk_iter_decoder;
 mod ssz_shim;
 
 pub use borsh_shim::*;
+pub use chunk_iter_decoder::ChunkIterDecoder;
 pub use ssz_shim::*;
